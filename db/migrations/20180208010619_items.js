2326b53db93dcf5f398a7d261038280a305ccf31
Taylor <thulsmans.2994@me.com>
2018-02-08 01:58:21 +0000

    fixed syntax ins users migration

--- db/migrations/20180208010619_items.js
+++ db/migrations/20180208010619_items.js
@@ -1,8 +1,21 @@
 //makes items table
 exports.up = function(knex, Promise) {
+return Promise.all([
+    knex.schema.table('items', function(table) {
+      table.increment();
+      table.integer('restaurant_id').unsigned();
+      table.foreign('restaurant_id').references('restaurant_id');
+      table.string('name');
+      table.numeric('price');
+      table.string('description');
+      table.string('pictures');
+      table.integer('likes');
 
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-
+  return Promise.all([
+    knex.schema.dropTable('items')]);
 };
--- db/migrations/20180208010730_users.js
+++ db/migrations/20180208010730_users.js
@@ -1,19 +1,23 @@
 //add users table
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('users', function(table){
-      table.incremnets();
-      table.unique('username');
+    knex.schema.createTable('users', function(table) {
+      table.increments();
+      table.string('username');
       table.string('password');
-      table.unique('email');
+      table.string('email');
+      table.unique(['email']);
       table.string('address');
 
       table.timestamps();
+    })
+  ]);
 };
+
 
 exports.down = function(knex, Promise) {
 
   return Promise.all([
     knex.schema.dropTable('users')
-  ])
+  ]);
 };
