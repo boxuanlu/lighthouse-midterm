9191a8bc64d9ce53c42e5aa28937cfa37decb442
ravindra rawat <ravinrawat.wd@gmail.com>
2018-02-08 17:30:00 +0000

    Trying to commit before pull

--- db/migrations/20180208010510_restaurants.js
+++ db/migrations/20180208010510_restaurants.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('restaurants', function (table) {
     table.increments();
     table.string('username').notNullable()
-    table.unique(['username']).notNullable();
+    table.unique(['username']);
     table.string('password').notNullable();
     table.string('email').notNullable();
   });
