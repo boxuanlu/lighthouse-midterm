4d4b1c03499c35168dca3e521098dc8b5ed511bc
ravindra rawat <ravinrawat.wd@gmail.com>
2018-02-08 01:13:54 +0000

    restaurant migration table created

--- db/migrations/20180208010510_restaurants.js
+++ db/migrations/20180208010510_restaurants.js
@@ -1,8 +1,13 @@
 // creates restaurants table
 exports.up = function(knex, Promise) {
-
+  return knex.schema.createTable('restaurants', function (table) {
+    table.increments();
+    table.string('username');
+    table.string('password');
+    table.string('email');
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('restaurants');
 };
