0a968382c4924cbffdff501c7d77128a8536ddb6
Taylor <thulsmans.2994@me.com>
2018-02-09 03:13:54 +0000

    added basic add button functionality

--- public/scripts/addItem.js
+++ public/scripts/addItem.js
@@ -1,8 +1,35 @@
 $(document).ready(function() {
   const addButtons = $('.add-item');
-  addButtons.addEventListener(function() {
+  addButtons.click(function() {
 
+    // price of parent .price and name of parent .name
+    const price = $(this).siblings('.price').text();
+    const name = $(this).siblings('.name').text();
+    console.log(name);
+
+    const row = $('<tr/>', {
+      'class': 'order-item'
+    });
+    console.log(row)
+    const itemName = $('<td/>', {
+      'class': 'order-name'
+    });
+    $(itemName).append(document.createTextNode(name));
+    $(row).append(itemName);
+    const itemPrice = $('<td/>', {
+      'class': 'order-price'
+    });
+    $(itemPrice).append(document.createTextNode(price));
+    $(row).append(itemPrice);
+    $('.order-list').append(row);
+
+    $();
   });
 });
 
 
+/*     <tr>
+        <th>Order Item Name</th>
+        <td>Quantity</td>
+        <td>Item price</td>
+      </tr>*/
