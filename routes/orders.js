14e36b43bb5af792164cedc91a87b1432c530a45
Taylor <thulsmans.2994@me.com>
2018-02-10 23:23:17 +0000

    working posting order to database

--- routes/orders.js
+++ routes/orders.js
@@ -8,7 +8,18 @@ module.exports = (knex) => {
   router.post('/', (req, res) => {
     console.log(req.body);
     // Todo post order to database
-    res.redirect('/')
+    knex('orders').insert({subtotal: req.body['order-subtotal'], tax: req.body['order-taxes'],
+      total: req.body['order-total'], comment: 'this is a comment' }).returning('id').into('orders')
+    .then (function (id) {
+        let orderId = id[0];
+        return knex.select('id').from('items').where('name', req.body['order-food']).then(function (foodId) {
+          let itemId = foodId[0].id
+
+            res.redirect('/')
+        })      })
+
+
+
 
   });
   return router;
